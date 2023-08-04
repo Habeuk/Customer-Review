7cfd216bb82c8fef7b663c706a456bd897ab32c1
Justin-Jordan <justi2017@outlook.fr>
2023-08-04 08:50:07 +0100

    add shopify installation flow

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Api\ShopifyApiGateway;
+use App\Entity\Shop;
+use Doctrine\ORM\EntityManagerInterface;
+use Shopify\Auth\FileSessionStorage;
+use Shopify\Auth\OAuth;
+use Shopify\Context;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class HomeController extends AbstractController
+{
+    #[Route('api/oauth/install', name: 'app_home')]
+    public function index(Request $request): Response
+    {
+        $hmac = $request->get("hmac");
+        $shop = $request->get('shop');
+        $key = $this->getParameter('shopify_api_key');
+        $secret = $this->getParameter('shopify_api_secret');
+        $scope = $this->getParameter('shopify_app_scope');
+
+        $nonce = "hello";
+        $redirectUri = "https://4d65-102-244-45-209.ngrok-free.app" . $this->generateUrl('app_auth_redirect');
+
+        $shopifyRedirect = "https://$shop/admin/oauth/authorize?client_id=$key&scope=$scope&redirect_uri=$redirectUri&state=$nonce&grant_options[]=$scope";
+
+        $response = new RedirectResponse($shopifyRedirect,);
+        $response->headers->setCookie(Cookie::create($nonce, $nonce, domain: "$shop.myshopify.com"));
+        //dd($response);
+
+        return $response;
+    }
+
+    #[Route('api/oauth/redirect', name: 'app_auth_redirect')]
+    public function action(Request $request, HttpClientInterface $client, EntityManagerInterface $em): Response
+    {
+        $hmac = $request->get("hmac");
+
+        $key = $this->getParameter('shopify_api_key');
+        $secret = $this->getParameter('shopify_api_secret');
+
+        $params = $request->query->all();
+        $params = array_diff_key($params, array('hmac' => ''));
+        ksort($params);
+
+        $computedHmac = hash_hmac('sha256', http_build_query($params), $secret);
+        if (hash_equals($hmac, $computedHmac)) {
+            $accessTokenUrl = "https://" . $params['shop'] . "/admin/oauth/access_token";
+            $data = [
+                'client_id' => $key,
+                'client_secret' => $secret,
+                'code' => $params['code'],
+            ];
+            $response = $client->request(
+                Request::METHOD_POST,
+                $accessTokenUrl,
+                [
+                    'body' => $data,
+                ]
+            );
+
+            $content = $response->toArray();
+            $token = $content["access_token"];
+
+            $shop = new Shop();
+
+            $shop->setName($params['shop']);
+            $shop->setToken($token);
+
+            $em->persist($shop);
+            $em->flush();
+
+            return $this->redirect('/');
+        }
+
+        return new Response('', Response::HTTP_BAD_REQUEST);
+    }
+}
---
+++ src/Entity/Shop.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShopRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ShopRepository::class)]
+class Shop
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $token = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(?string $token): static
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
